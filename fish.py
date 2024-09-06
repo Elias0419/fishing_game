791423be94ec6bf6c28ba4440597470c4cae2ff7
Elias0419 <operryb1@gmail.com>
2024-09-05 21:33:27 -0400

    add more fish

--- fish.py
+++ fish.py
@@ -47,7 +47,7 @@ class Goldfish(Fish):
     #     self.gives_exp: int = random.randint(3, 10)
     #     super().__post_init__()
 
-
+#######################################################################################################
 @dataclass
 class Bluegill(Fish):
     name: str = "Bluegill"
@@ -190,3 +190,217 @@ class GreatWhite(Fish):
     minimum_fishing_experience: int = 100
     specific_type: str = "great_white"
     subtype: str = "sharks"
+
+#
+
+@dataclass
+class Perch(Fish):
+    name: str = "Perch"
+    specific_type: str = "perch"
+    subtype: str = "bony_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(20, 35)
+        self.length_inch = random.uniform(6, 12)
+        self.gives_exp = random.randint(8, 15)
+        super().__post_init__()
+
+@dataclass
+class Walleye(Fish):
+    name: str = "Walleye"
+    specific_type: str = "walleye"
+    subtype: str = "bony_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(35, 55)
+        self.length_inch = random.uniform(15, 30)
+        self.gives_exp = random.randint(18, 28)
+        super().__post_init__()
+
+@dataclass
+class Muskie(Fish):
+    name: str = "Muskie"
+    specific_type: str = "muskie"
+    subtype: str = "bony_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(60, 80)
+        self.length_inch = random.uniform(30, 50)
+        self.gives_exp = random.randint(25, 35)
+        super().__post_init__()
+
+
+@dataclass
+class Marlin(Fish):
+    name: str = "Marlin"
+    specific_type: str = "marlin"
+    subtype: str = "billfish"
+
+    def __post_init__(self):
+        self.stamina = random.randint(80, 100)
+        self.length_inch = random.uniform(60, 120)
+        self.gives_exp = random.randint(35, 45)
+        super().__post_init__()
+
+####
+
+@dataclass
+class Barracuda(Fish):
+    name: str = "Barracuda"
+    specific_type: str = "barracuda"
+    subtype: str = "predatory_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(50, 70)
+        self.length_inch = random.uniform(20, 40)
+        self.gives_exp = random.randint(20, 30)
+        super().__post_init__()
+
+@dataclass
+class Eel(Fish):
+    name: str = "Eel"
+    specific_type: str = "eel"
+    subtype: str = "elongated_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(30, 50)
+        self.length_inch = random.uniform(20, 50)
+        self.gives_exp = random.randint(15, 25)
+        super().__post_init__()
+
+@dataclass
+class Clownfish(Fish):
+    name: str = "Clownfish"
+    specific_type: str = "clownfish"
+    subtype: str = "tropical_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(10, 20)
+        self.length_inch = random.uniform(3, 6)
+        self.gives_exp = random.randint(5, 10)
+        super().__post_init__()
+
+@dataclass
+class GoliathGrouper(Fish):
+    name: str = "Goliath Grouper"
+    specific_type: str = "goliath_grouper"
+    subtype: str = "large_fishes"
+
+    def __post_init__(self):
+        self.stamina = random.randint(70, 100)
+        self.length_inch = random.uniform(50, 100)
+        self.gives_exp = random.randint(30, 50)
+        super().__post_init__()
+
+
+
+#####
+
+@dataclass
+class Tuna(Fish):
+    name: str = "Tuna"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Cod(Fish):
+    name: str = "Cod"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Halibut(Fish):
+    name: str = "Halibut"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Mackerel(Fish):
+    name: str = "Mackerel"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Perch(Fish):
+    name: str = "Perch"
+
+    def __post_init__(self):
+        pass
+
+
+
+@dataclass
+class Swordfish(Fish):
+    name: str = "Swordfish"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Tilapia(Fish):
+    name: str = "Tilapia"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Sardine(Fish):
+    name: str = "Sardine"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Anchovy(Fish):
+    name: str = "Anchovy"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Pike(Fish):
+    name: str = "Pike"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Flounder(Fish):
+    name: str = "Flounder"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Grouper(Fish):
+    name: str = "Grouper"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Haddock(Fish):
+    name: str = "Haddock"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Mahi(Fish):
+    name: str = "Mahi-Mahi"
+
+    def __post_init__(self):
+        pass
+
+@dataclass
+class Snapper(Fish):
+    name: str = "Snapper"
+
+    def __post_init__(self):
+        pass
+
+
